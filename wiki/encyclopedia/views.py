284b8e104f38a27e997215da2a01f8353ce517ce
Eric Hashimoto <66524062+Abac-axi@users.noreply.github.com>
2020-07-25 15:23:14 -0300

    SEARCH HALF MAKE!!

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -4,7 +4,52 @@ from . import util
 
 
 def index(request):
- 
+    if request.method == "POST":
+        input = request.POST["q"]
+        #print('the input is: ' + input)
+        entries = util.list_entries()
+        results = []
+        
+        for entry in entries:
+            #print('analysing entry '+entry)
+            if entry.lower() == input.lower():
+                content = util.get_entry(entry)
+                return render(request, "encyclopedia/entrypage.html", {
+                    "title": input.capitalize(),
+                    "content": content
+                })
+            else:
+                i=0
+                for c in input.lower():
+                    if c == entry.lower()[i]:
+                        results.append(entry) #TODO c is only checkin gthe first letter
+                    else:
+                        break
+                    i += 1
+
+
+
+        
+            
+        
+        
+
+            
+
+        return render(request, "encyclopedia/searchpage.html", {
+            "input": input,
+            "results": results
+        })
+        
+        
+
+
+        return render(request, "encyclopedia/searchpage.html", {
+            "input": input,
+            "results": []
+    
+        })
+
 
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
